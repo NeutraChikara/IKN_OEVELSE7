21bcd8e94599a53b731d06a2f683d315da019ef6
root <antonzxcz@yahoo.dk>
2017-03-15 11:16:19 +0100

    first start

--- file_client/file_client.cs
+++ file_client/file_client.cs
@@ -27,8 +27,7 @@ namespace tcp
 		{
 			string spacer = "--------------------";
 			// Sets up connection and calls receiveFile
-			TcpClient clientSocket = new TcpClient();
-			clientSocket.ReceiveBufferSize = 2240;
+			UdpClient clientSocket = new UdpClient();
 
 			while (true) 
 			{
@@ -36,8 +35,7 @@ namespace tcp
 				{
 					// Setting up connection and calling receiveFile
 					clientSocket.Connect (args [0], PORT);
-					NetworkStream serverStream = clientSocket.GetStream ();
-					receiveFile (args [1], serverStream);
+
 					clientSocket.Close ();
 					break;
 				}
@@ -63,63 +61,11 @@ namespace tcp
 				
 			}
 		}
-
-		/// <summary>
-		/// Receives the file.
-		/// </summary>
-		/// <param name='fileName'>
-		/// File name.
-		/// </param>
-		/// <param name='io'>
-		/// Network stream for reading from the server
-		/// </param>
-		private void receiveFile (String fileName, NetworkStream io)
+		private void SendChar(char charToSend,UdpClient udpclient)
 		{
-			fileName = fileName + "$";
+			byte byteToSend = Convert.ToByte (charToSend);
 
-			// Send text with path and name of file
-			byte[] outStream = System.Text.Encoding.ASCII.GetBytes(fileName);
-			io.Write(outStream, 0, outStream.Length);
-			// io.Flush();		// As of right now, this does nothing on a NetworkStream
-
-			// Receive size of file
-			byte[] msgLengthBytes = new byte[sizeof(long)];	// Or hardcode size of a double in bytes manually
-			io.Read(msgLengthBytes, 0, msgLengthBytes.Length);
-			long msgLength = BitConverter.ToInt64(msgLengthBytes, 0);
-
-			// If file was not found
-			if (msgLength == -1)
-			{
-				Console.WriteLine ("ERROR: File was not found on server");
-				return;
-			}
-				
-			// Commence download
-			Console.WriteLine("Downloading...");
-
-			byte[] inStream = new byte[BUFSIZE];
-			long totalReceived = 0;
-
-			// Setup FileStream which writes to desktop
-			string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/" + LIB.extractFileName(fileName);
-			FileStream fs = new FileStream (path, FileMode.Create);
-
-			// Download file chunk by chunk and write it to FileStream path
-			do
-			{
-				int count = io.Read (inStream, 0, BUFSIZE);
-			
-
-				fs.Write(inStream, 0, count);
-				totalReceived += count;
-
-				string diff = totalReceived + "/" + msgLength;
-
-				Console.Write("\r{0}%   " + diff + " bytes", String.Format("{0:0}", 100*totalReceived/msgLength));
-			}
-			while (totalReceived != msgLength);
-
-			Console.WriteLine ("\nDownload complete");
+			udpclient.BeginSend (byteToSend, 1, udpclient.EndSend);
 		}
 
 		/// <summary>
